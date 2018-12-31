8b6ec6b8f42508e0c39f1221a7ded5178784345f
Robert Wang <robert.lu.wang@gmail.com>
2018-12-31 07:20:56 -0500

    Create macos_mojave_iso.sh

---
+++ macos_mojave_iso.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# script to build macos mojave iso 
+# Dec 31,2018
+#
+
+
+hdiutil create -o /tmp/Mojave.cdr -size 7000m -layout SPUD -fs HFS+J
+hdiutil attach /tmp/Mojave.cdr.dmg -noverify -mountpoint /Volumes/install_build
+sudo /Applications/Install\ macOS\ Mojave.app/Contents/Resources/createinstallmedia --volume /Volumes/install_build
+mv /tmp/Mojave.cdr.dmg ~/Desktop/InstallSystem.dmg
+hdiutil detach /Volumes/Install\ macOS\ Mojave
+hdiutil convert ~/Desktop/InstallSystem.dmg -format UDTO -o ~/Desktop/Mojave.iso
+rm ~/Desktop/InstallSystem.dmg
