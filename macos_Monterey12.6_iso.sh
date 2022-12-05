e27fcb40d4eb5e5000796bb231617ef7fbe69679
Robert Wang <robert.lu.wang@gmail.com>
2022-12-05 12:12:28 -0500

    Update macos_Monterey12.6_iso.sh

--- macos_Monterey12.6_iso.sh
+++ macos_Monterey12.6_iso.sh
@@ -1,4 +1,4 @@
-hdiutil create -o /tmp/Monterey.cdr -size 13000m -layout SPUD -fs HFS+J
+hdiutil create -o /tmp/Monterey.cdr -size 14000m -layout SPUD -fs HFS+J
 hdiutil attach /tmp/Monterey.cdr.dmg -noverify -mountpoint /Volumes/install_build
 sudo /Applications/Install\ macOS\ Monterey.app/Contents/Resources/createinstallmedia --volume /Volumes/install_build
 mv /tmp/Monterey.cdr.dmg ~/Desktop/InstallSystem.dmg
